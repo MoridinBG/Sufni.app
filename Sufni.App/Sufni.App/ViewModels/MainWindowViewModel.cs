62d995f34ed4dfe541012dd68767b441b514ac40
sghctoma <sghctoma@gmail.com>
2025-05-07 14:34:40 +0200

    Avoid creating duplicate MainPagesViewModel

--- Sufni.App/Sufni.App/ViewModels/MainWindowViewModel.cs
+++ Sufni.App/Sufni.App/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,23 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Sufni.App.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     [ObservableProperty] private ViewModelBase currentView;
-    [ObservableProperty] private MainPagesViewModel mainPagesViewModel = new();
+    [ObservableProperty] private MainPagesViewModel? mainPagesViewModel;
 
     private readonly Stack<ViewModelBase> viewHistory = new();
     private readonly WelcomeScreenViewModel welcomeScreenViewModel = new();
 
     public MainWindowViewModel()
     {
+        mainPagesViewModel = App.Current?.Services?.GetService<MainPagesViewModel>();
+        Debug.Assert(mainPagesViewModel != null, nameof(mainPagesViewModel) + " != null");
+
         CurrentView = welcomeScreenViewModel;
     }
 
