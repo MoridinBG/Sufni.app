1f96852f48501b249341125ee33c03c8ca243562
sghctoma <sghctoma@gmail.com>
2025-06-06 21:06:02 +0200

    Make the left pane hideable

--- Sufni.App/Sufni.App/DesktopViews/MainPagesDesktopView.axaml.cs
+++ Sufni.App/Sufni.App/DesktopViews/MainPagesDesktopView.axaml.cs
@@ -1,6 +1,7 @@
-using System.Linq;
+using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.LogicalTree;
+using Avalonia.VisualTree;
 using Sufni.App.Views;
 
 namespace Sufni.App.DesktopViews;
@@ -26,5 +27,20 @@ public partial class MainPagesDesktopView : MainPagesViewBase
                 };
             }
         };
+        // Allow the pane to close/open on tab header clicks
+        var pagesTabItems = PagesMenu.GetLogicalChildren();
+        foreach (var item in pagesTabItems)
+        {
+            var tabItem = item as TabItem;
+            Debug.Assert(tabItem is not null);
+
+            tabItem.PointerPressed += (_, _) =>
+            {
+                var splitView = PagesMenu.FindAncestorOfType<SplitView>();
+                Debug.Assert(splitView is not null);
+
+                splitView.IsPaneOpen = tabItem != PagesMenu.SelectedItem || !splitView.IsPaneOpen;
+            };
+        }
     }
 }
