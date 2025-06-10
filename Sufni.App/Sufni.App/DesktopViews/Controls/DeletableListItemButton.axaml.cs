354fb2138fe7c5d860a92e14badaa5aa10139c87
sghctoma <sghctoma@gmail.com>
2025-06-10 16:25:03 +0200

    Gray out the trash icon for undeletable items

--- Sufni.App/Sufni.App/DesktopViews/Controls/DeletableListItemButton.axaml.cs
+++ Sufni.App/Sufni.App/DesktopViews/Controls/DeletableListItemButton.axaml.cs
@@ -7,5 +7,13 @@ public partial class DeletableListItemButton : UserControl
     public DeletableListItemButton()
     {
         InitializeComponent();
+
+        PropertyChanged += (_, e) =>
+        {
+            if (e.Property.Name != nameof(IsEnabled)) return;
+
+            var css = IsEnabled ? ".default { fill: #bf312d; }" : ".default { fill: #f0f0f0; }";
+            DeleteButton.SetCurrentValue(Avalonia.Svg.Skia.Svg.CssProperty, css);
+        };
     }
 }
