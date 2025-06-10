e97e3f05748999b7a593a78a212916d5479e666f
sghctoma <sghctoma@gmail.com>
2025-06-10 16:26:10 +0200

    Make notifications auto-disappear after a timeout

--- Sufni.App/Sufni.App/ViewModels/ViewModelBase.cs
+++ Sufni.App/Sufni.App/ViewModels/ViewModelBase.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Diagnostics;
+using System.Timers;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
@@ -8,12 +10,57 @@ namespace Sufni.App.ViewModels
 {
     public partial class ViewModelBase : ObservableObject
     {
+        private const int Timeout = 3000;
+
         #region Observable properties
 
+        [ObservableProperty] private bool isPointerOverNotifications;
+        
         public ObservableCollection<string> ErrorMessages { get; } = [];
         public ObservableCollection<string> Notifications { get; } = [];
 
         #endregion Observable properties
+        
+        #region Private fields
+        
+        // Timer for notifications. They are closed 3 seconds after the last messages arrived,unless
+        // the pointer is over them. In such case, the timer starts when the pointer leaves.
+        private readonly Timer notificationsTimer = new(Timeout);
+
+        #endregion Private fields
+
+        #region Property change handlers
+
+        partial void OnIsPointerOverNotificationsChanged(bool value)
+        {
+            // Reset the timer when the pointer leaves the notifications container
+            if (value) return;
+            notificationsTimer.Stop();
+            notificationsTimer.Start();
+        }
+
+        #endregion Property change handlers
+
+        #region Constructors
+
+        protected ViewModelBase()
+        {
+            notificationsTimer.AutoReset = false;
+            notificationsTimer.Elapsed += (_, _) =>
+            {
+                if (IsPointerOverNotifications) return;
+                Notifications.Clear();
+            };
+
+            Notifications.CollectionChanged += (_, args) =>
+            {
+                if (args.Action != NotifyCollectionChangedAction.Add) return;
+                notificationsTimer.Stop();
+                notificationsTimer.Start();
+            };
+        }
+
+        #endregion Constructors
 
         #region Commands
 
