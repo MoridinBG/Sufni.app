1d1820a2ba5543ca39fd6736f5e81d2cd663851c
sghctoma <sghctoma@gmail.com>
2025-06-20 18:23:26 +0200

    Add desktop-specific import sessions view

---
+++ Sufni.App/Sufni.App/DesktopViews/ImportSessionsDesktopView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Sufni.App.DesktopViews;
+
+public partial class ImportSessionsDesktopView : UserControl
+{
+    public ImportSessionsDesktopView()
+    {
+        InitializeComponent();
+    }
+}
