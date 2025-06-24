ed195fdbd7cac537638fa9a7b6eb0e81c1d6d9fb
sghctoma <sghctoma@gmail.com>
2025-06-24 14:39:44 +0200

    Add SyncServer project
    Creating the server that serves the HTTP API needs to be in a separate
    project, since it needs Microsoft.NET.Sdk.Web as Sdk. The actual
    endpoints will be implemented in Sufni.App, so that we can use the
    database, the models, etc.

---
+++ SyncServer/ApiHost.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SyncServer;
+
+public static class ApiHost
+{
+    public static WebApplication BuildApp(string jwtSecret)
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.ConfigureKestrel(options =>
+        {
+            options.ListenLocalhost(1557);
+        });
+
+        var key = Encoding.ASCII.GetBytes(jwtSecret);
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                RequireExpirationTime = true,
+                ValidateLifetime = true
+            };
+        });
+
+        builder.Services.AddAuthorization();
+
+        var app = builder.Build();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return app;
+    }
+}
