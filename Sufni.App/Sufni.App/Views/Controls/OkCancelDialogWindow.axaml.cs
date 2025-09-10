7d7bfaeab25a1c600b4862fe01f4617830897865
sghctoma <sghctoma@gmail.com>
2025-09-10 06:48:29 +0200

    Add parameterless constructor t OkCancelDialogWindow

--- Sufni.App/Sufni.App/Views/Controls/OkCancelDialogWindow.axaml.cs
+++ Sufni.App/Sufni.App/Views/Controls/OkCancelDialogWindow.axaml.cs
@@ -2,6 +2,8 @@ namespace Sufni.App.Views.Controls;
 
 public partial class OkCancelDialogWindow : DialogWindow
 {
+    public OkCancelDialogWindow() : base("Dialog") { }
+
     public OkCancelDialogWindow(string title, string message) : base(title)
     {
         InitializeComponent();
