5d9339ceb7afb1413e6077f5ec41822efb83ddd6
sghctoma <sghctoma@gmail.com>
2025-12-07 19:01:43 +0100

    Handle EncryptedSharedPreferences deprecation

--- SecureStorage/SecureStorage.android.cs
+++ SecureStorage/SecureStorage.android.cs
@@ -1,75 +1,117 @@
 using System.Diagnostics;
 using Android.Content;
-using AndroidX.Security.Crypto;
-using Xamarin.Google.Crypto.Tink.Shaded.Protobuf;
+using Android.Security.Keystore;
+using Java.Security;
+using Javax.Crypto;
+using Javax.Crypto.Spec;
 
 namespace SecureStorage;
 
+public class SecureStorageException : Exception;
+
 public class SecureStorage : ISecureStorage
 {
-    private const string Alias = "sufni.bridge.android.preferences";
-    private ISharedPreferences? sharedPreferences;
+    private const string KeyAlias = "sufni.app.android.keystore.aes";
+    private const string PrefName = "sufni.app.prefs";
+
+    private readonly ISharedPreferences? prefs;
 
     public SecureStorage()
     {
-        _ = InitAsync();
+        var context = Application.Context;
+        prefs = context.GetSharedPreferences(PrefName, FileCreationMode.Private);
+        EnsureKey();
     }
 
-    private async Task InitAsync()
+    private static void EnsureKey()
     {
-        sharedPreferences = await GetEncryptedSharedPreferences();
+        var keyStore = KeyStore.GetInstance("AndroidKeyStore");
+        Debug.Assert(keyStore is not null);
+        keyStore.Load(null);
+
+        if (keyStore.ContainsAlias(KeyAlias)) return;
+        var keyGenerator = KeyGenerator.GetInstance(KeyProperties.KeyAlgorithmAes, "AndroidKeyStore");
+        Debug.Assert(keyGenerator is not null);
+
+        var builder = new KeyGenParameterSpec.Builder(
+                KeyAlias,
+                KeyStorePurpose.Encrypt | KeyStorePurpose.Decrypt)
+            .SetBlockModes(KeyProperties.BlockModeGcm)
+            .SetEncryptionPaddings(KeyProperties.EncryptionPaddingNone)
+            .SetRandomizedEncryptionRequired(true);
+
+        keyGenerator.Init(builder.Build());
+        keyGenerator.GenerateKey();
     }
 
-    private async Task<ISharedPreferences?> GetEncryptedSharedPreferences()
+    private static ISecretKey? GetSecretKey()
     {
-        Debug.Assert(EncryptedSharedPreferences.PrefValueEncryptionScheme.Aes256Gcm != null,
-            "EncryptedSharedPreferences.PrefValueEncryptionScheme.Aes256Gcm != null");
-        Debug.Assert(EncryptedSharedPreferences.PrefKeyEncryptionScheme.Aes256Siv != null,
-            "EncryptedSharedPreferences.PrefKeyEncryptionScheme.Aes256Siv != null");
+        var keyStore = KeyStore.GetInstance("AndroidKeyStore");
 
+        keyStore?.Load(null);
+        return (ISecretKey?)keyStore?.GetKey(KeyAlias, null);
+    }
+
+    private static (byte[]? iv, byte[]? ciphertext) Encrypt(byte[] plain)
+    {
+        var cipher = Cipher.GetInstance("AES/GCM/NoPadding");
+
+        cipher?.Init(CipherMode.EncryptMode, GetSecretKey());
+
+        var iv = cipher?.GetIV();
+        var ciphertext = cipher?.DoFinal(plain);
+        return (iv, ciphertext);
+    }
+
+    private static byte[]? Decrypt(byte[] iv, byte[] ciphertext)
+    {
         try
         {
-            var context = Application.Context;
-            var prefsMainKey = new MasterKey.Builder(context, Alias)
-                .SetKeyScheme(MasterKey.KeyScheme.Aes256Gcm!)
-                .Build();
-            var pref = EncryptedSharedPreferences.Create(
-                context,
-                Alias,
-                prefsMainKey,
-                EncryptedSharedPreferences.PrefKeyEncryptionScheme.Aes256Siv,
-                EncryptedSharedPreferences.PrefValueEncryptionScheme.Aes256Gcm);
+            var spec = new GCMParameterSpec(128, iv);
+            var cipher = Cipher.GetInstance("AES/GCM/NoPadding");
 
-            return pref;
+            cipher?.Init(CipherMode.DecryptMode, GetSecretKey(), spec);
+            return cipher?.DoFinal(ciphertext);
         }
-        catch (InvalidProtocolBufferException)
+        catch
         {
-            await RemoveAllAsync();
             return null;
         }
     }
 
     public Task<byte[]?> GetAsync(string key)
     {
-        var value = sharedPreferences?.GetString(key, null);
-        return Task.FromResult(value is null ? null : Convert.FromBase64String(value));
+        var ivB64 = prefs?.GetString(key + "_iv", null);
+        var ctB64 = prefs?.GetString(key + "_ct", null);
+        if (ivB64 == null || ctB64 == null)
+            return Task.FromResult<byte[]?>(null);
+
+        var iv = Convert.FromBase64String(ivB64);
+        var ct = Convert.FromBase64String(ctB64);
+        return Task.FromResult(Decrypt(iv, ct));
     }
 
-    public Task<string?> GetStringAsync(string key)
+    public async Task<string?> GetStringAsync(string key)
     {
-        return Task.FromResult(sharedPreferences?.GetString(key, null));
+        var b = await GetAsync(key);
+        return b == null ? null : System.Text.Encoding.UTF8.GetString(b);
     }
 
     public Task SetAsync(string key, byte[]? value)
     {
-        using var editor = sharedPreferences?.Edit();
-        if (value is null)
+        var editor = prefs?.Edit();
+
+        if (value == null)
         {
-            editor?.Remove(key);
+            editor?.Remove(key + "_iv");
+            editor?.Remove(key + "_ct");
         }
         else
         {
-            editor?.PutString(key, Convert.ToBase64String(value));
+            var (iv, ct) = Encrypt(value);
+            if (iv is null || ct == null) return Task.FromException(new SecureStorageException());
+            editor?.PutString(key + "_iv", Convert.ToBase64String(iv));
+            editor?.PutString(key + "_ct", Convert.ToBase64String(ct));
         }
 
         editor?.Apply();
@@ -78,31 +120,21 @@ public class SecureStorage : ISecureStorage
 
     public Task SetStringAsync(string key, string? value)
     {
-        using var editor = sharedPreferences?.Edit();
-        if (value is null)
-        {
-            editor?.Remove(key);
-        }
-        else
-        {
-            editor?.PutString(key, value);
-        }
-
-        editor?.Apply();
-        return Task.CompletedTask;
+        return SetAsync(key, value == null ? null : System.Text.Encoding.UTF8.GetBytes(value));
     }
 
     public Task RemoveAsync(string key)
     {
-        using var editor = sharedPreferences?.Edit();
-        editor?.Remove(key);
+        var editor = prefs?.Edit();
+        editor?.Remove(key + "_iv");
+        editor?.Remove(key + "_ct");
         editor?.Apply();
         return Task.CompletedTask;
     }
 
     public Task RemoveAllAsync()
     {
-        using var editor = sharedPreferences?.Edit();
+        var editor = prefs?.Edit();
         editor?.Clear();
         editor?.Apply();
         return Task.CompletedTask;
