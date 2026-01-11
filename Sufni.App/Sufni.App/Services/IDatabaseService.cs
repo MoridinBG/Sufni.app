d3df1aa74691ea62bd8e63aa88d431ba5e437125
Ivan Dilchovski <root.darkstar@gmail.com>
2026-01-11 17:05:57 +0200

    Add AppSetting model to the db to persist key-value app preferences & settings

--- Sufni.App/Sufni.App/Services/IDatabaseService.cs
+++ Sufni.App/Sufni.App/Services/IDatabaseService.cs
@@ -33,5 +33,7 @@ public interface IDatabaseService
     public Task<PairedDevice?> GetPairedDeviceByTokenAsync(string token);
     public Task PutPairedDeviceAsync(PairedDevice device);
     public Task DeletePairedDeviceAsync(string id);
+    public Task<string?> GetAppSettingAsync(string key);
+    public Task PutAppSettingAsync(string key, string value);
     public Task MergeAllAsync(SynchronizationData data);
 }
