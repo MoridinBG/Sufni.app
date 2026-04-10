b1c226772932500d1930cb31f2b57f8b01c08279
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-10 14:07:47 +0300

    Move haptic feedback into mobile heads

--- Sufni.App/Sufni.App.iOS/AppDelegate.cs
+++ Sufni.App/Sufni.App.iOS/AppDelegate.cs
@@ -2,7 +2,6 @@ using Avalonia;
 using Avalonia.iOS;
 using Foundation;
 using FriendlyNameProvider;
-using HapticFeedback;
 using Microsoft.Extensions.DependencyInjection;
 using SecureStorage;
 using ServiceDiscovery;
@@ -24,7 +23,7 @@ namespace Sufni.App.iOS
             App.ServiceCollection.AddSingleton<IFriendlyNameProvider, FriendlyNameProvider.FriendlyNameProvider>();
             App.ServiceCollection.AddKeyedSingleton<IServiceDiscovery, ServiceDiscovery.ServiceDiscovery>("gosst");
             App.ServiceCollection.AddKeyedSingleton<IServiceDiscovery, ServiceDiscovery.ServiceDiscovery>("sync");
-            App.ServiceCollection.AddSingleton<IHapticFeedback, HapticFeedback.HapticFeedback>();
+            App.ServiceCollection.AddSingleton<IHapticFeedback, IosHapticFeedback>();
             App.ServiceCollection.AddSingleton<ISynchronizationClientService, SynchronizationClientService>();
             App.ServiceCollection.AddSingleton<IPairingClientCoordinator, PairingClientCoordinator>();
             App.ServiceCollection.AddSingleton<PairingClientViewModel>();
