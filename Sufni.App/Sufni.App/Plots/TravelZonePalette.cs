d960ce8db99552bce08510ca96765f8ba592d56b
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-18 14:27:40 +0300

    Unify session statistics plot views

---
+++ Sufni.App/Sufni.App/Plots/TravelZonePalette.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Sufni.App.Plots;
+
+public static class TravelZonePalette
+{
+    public static IReadOnlyList<string> HexColors { get; } =
+    [
+        "#3288bd",
+        "#66c2a5",
+        "#abdda4",
+        "#e6f598",
+        "#ffffbf",
+        "#fee08b",
+        "#fdae61",
+        "#f46d43",
+        "#d53e4f",
+        "#9e0142",
+    ];
+}
