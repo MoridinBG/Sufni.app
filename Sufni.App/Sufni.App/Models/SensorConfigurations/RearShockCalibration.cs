be295c0db3c1d4cea929dd9ade08562afcccb445
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-18 14:26:06 +0300

    Simplify rear travel calibration

--- Sufni.App/Sufni.App/Models/SensorConfigurations/RearShockCalibration.cs
+++ Sufni.App/Sufni.App/Models/SensorConfigurations/RearShockCalibration.cs
@@ -2,14 +2,6 @@ using System;
 
 namespace Sufni.App.Models.SensorConfigurations;
 
-internal sealed record RearShockCalibration(
-    double MaxShockStroke,
-    Func<ushort, double> MeasurementToShockStroke);
-
-internal sealed record RearSuspensionMapping(
-    double MaxWheelTravel,
-    Func<double, double> ShockStrokeToWheelTravel);
-
 internal sealed record RearTravelCalibration(
     double MaxTravel,
     Func<ushort, double> MeasurementToTravel);
