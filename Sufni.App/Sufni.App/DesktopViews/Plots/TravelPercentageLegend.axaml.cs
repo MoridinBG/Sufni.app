c0862f68815ceeed7ea89b0f22afdc764ab40e9f
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-18 14:27:40 +0300

    Unify session statistics plot views

--- Sufni.App/Sufni.App/DesktopViews/Plots/TravelPercentageLegend.axaml.cs
+++ Sufni.App/Sufni.App/DesktopViews/Plots/TravelPercentageLegend.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Media;
+using Sufni.App.Plots;
 
 namespace Sufni.App.DesktopViews.Plots;
 
@@ -7,5 +9,13 @@ public partial class TravelPercentageLegend : UserControl
     public TravelPercentageLegend()
     {
         InitializeComponent();
+
+        foreach (var color in TravelZonePalette.HexColors)
+        {
+            PaletteGrid.Children.Add(new Border
+            {
+                Background = new SolidColorBrush(Color.Parse(color))
+            });
+        }
     }
 }
