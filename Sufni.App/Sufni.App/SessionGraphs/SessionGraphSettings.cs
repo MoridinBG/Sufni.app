12250f32cb4539cb802d85d018f0c68e2be56607
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-25 00:25:11 +0300

    Adjust downsampling and refresh of live data

--- Sufni.App/Sufni.App/SessionGraphs/SessionGraphSettings.cs
+++ Sufni.App/Sufni.App/SessionGraphs/SessionGraphSettings.cs
@@ -2,8 +2,8 @@ namespace Sufni.App.SessionGraphs;
 
 public static class SessionGraphSettings
 {
-    public const int LiveGraphRefreshIntervalMs = 33;
+    public const int LiveGraphRefreshIntervalMs = 100;
     public const int LiveUiRefreshIntervalMs = 100;
     public const int LiveStatisticsRefreshIntervalMs = 1000;
-    public const int RecordedMobileMaximumDisplayHz = 1;
+    public const int RecordedMobileMaximumDisplayHz = 100;
 }
