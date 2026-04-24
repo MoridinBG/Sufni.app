ab148883bca192cc1b870050d5ab71c5524a4968
Ivan Dilchovski <root.darkstar@gmail.com>
2026-04-24 13:16:54 +0300

    Add mobile live DAQ diagnostics view

---
+++ Sufni.App/Sufni.App/Views/Editors/LiveDaqDetailView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Sufni.App.Views.Editors;
+
+public partial class LiveDaqDetailView : UserControl
+{
+    public LiveDaqDetailView()
+    {
+        InitializeComponent();
+    }
+}
