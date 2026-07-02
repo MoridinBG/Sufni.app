7e39bed9ef78c23a88ac6d6bb1d48c822176e9a6
Ivan Dilchovski <root.darkstar@gmail.com>
2026-07-02 20:54:54 +0300

    Build kinematics test linkages as specs

--- Sufni.Kinematics.Tests/TestLinkages.cs
+++ Sufni.Kinematics.Tests/TestLinkages.cs
@@ -4,32 +4,31 @@ namespace Sufni.Kinematics.Tests;
 
 internal static class TestLinkages
 {
-    public static Linkage FullSuspensionLinkage(bool includeHeadTubeJoints = false)
+    public static LinkageSpec FullSuspensionLinkageSpec(bool includeHeadTubeJoints = false)
     {
         var mapping = new JointNameMapping();
-        var bottomBracket = new Joint(mapping.BottomBracket, JointType.BottomBracket, 0, 0);
-        var rearWheel = new Joint(mapping.RearWheel, JointType.RearWheel, 4, 0);
-        var frontWheel = new Joint(mapping.FrontWheel, JointType.FrontWheel, 12, 1);
-        var shockEye1 = new Joint(mapping.ShockEye1, JointType.Floating, 4, 3);
-        var shockEye2 = new Joint(mapping.ShockEye2, JointType.Fixed, 0, 3);
 
-        List<Joint> joints = [bottomBracket, rearWheel, frontWheel, shockEye1, shockEye2];
+        List<JointSpec> joints =
+        [
+            new(mapping.BottomBracket, JointType.BottomBracket, 0, 0),
+            new(mapping.RearWheel, JointType.RearWheel, 4, 0),
+            new(mapping.FrontWheel, JointType.FrontWheel, 12, 1),
+            new(mapping.ShockEye1, JointType.Floating, 4, 3),
+            new(mapping.ShockEye2, JointType.Fixed, 0, 3)
+        ];
         if (includeHeadTubeJoints)
         {
-            joints.Add(new Joint(mapping.HeadTube1, JointType.HeadTube, 10, 2));
-            joints.Add(new Joint(mapping.HeadTube2, JointType.HeadTube, 9, 5));
+            joints.Add(new JointSpec(mapping.HeadTube1, JointType.HeadTube, 10, 2));
+            joints.Add(new JointSpec(mapping.HeadTube2, JointType.HeadTube, 9, 5));
         }
 
-        return Linkage.CreateResolved(
+        return new LinkageSpec(
             joints,
             [
-                new Link(bottomBracket, rearWheel),
-                new Link(rearWheel, shockEye1),
+                new LinkSpec(mapping.BottomBracket, mapping.RearWheel),
+                new LinkSpec(mapping.RearWheel, mapping.ShockEye1),
             ],
-            new Link(shockEye1, shockEye2),
+            new LinkSpec(mapping.ShockEye1, mapping.ShockEye2),
             0.5);
     }
-
-    public static LinkageSpec FullSuspensionLinkageSpec(bool includeHeadTubeJoints = false) =>
-        FullSuspensionLinkage(includeHeadTubeJoints).ToSpec();
 }
